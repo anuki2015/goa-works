a98185353b940680607234e85d9b571c9d76c4ef
anuki2015 <anukisologhashvili@gmail.com>
2025-02-12 21:43:59 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,11 @@
+name="anuki "
+surname="sologhashvili "
+age=9
+height = 126.5
+
+
+
+print(name)
+print (name+" "+surname)
+print(name+str(age)+" year")
+print("I am " + name +surname+str(age)+" year's old and my height is " + str(height))
