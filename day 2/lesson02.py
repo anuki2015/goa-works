79a55d40dcfe0933d56b4488704e3a8b9394d96e
anuki2015 <anukisologhashvili@gmail.com>
2025-03-02 17:35:16 +0400

    Changes in submodule

--- day 2/lesson02.py
+++
@@ -1,4 +0,0 @@
-print("this is integer- ", 20)
-print("this is string anuki")
-print("this is boolean - true or false")
-print ("this is float- " , 1.5)
--- day 2/lesson03.py
+++
@@ -1,6 +0,0 @@
-a = int(input("გთხოვ შეიყვანე a:"))
-b = int(input("გთხოვ შეიყვანე b:"))
-sum = a+b
-mult = a*b
-print ("a*b=" , mult)
-print ('a+b=', sum)
